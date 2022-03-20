2348c7bab833e908564006ca043b9a16cefc783f
Matheus <matheushenr941@gmail.com>
2022-03-20 12:37:55 -0300

    commit inicial

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import React, { useState } from "react";
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>Bookmark page</Head>
+    </div>
+  )
+}
