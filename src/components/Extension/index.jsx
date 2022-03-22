66fa1612f2dcdb56bf68e9b0adc3f958a6fb8014
Matheus <matheushenr941@gmail.com>
2022-03-22 19:37:44 -0300

    Finalizado component extension

--- src/components/Extension/index.jsx
+++ src/components/Extension/index.jsx
@@ -1,30 +1,55 @@
 import styles from "./extension.module.scss";
+import Image from "next/image";
 
 export default function Extension() {
   return (
     <div className={styles.component}>
       <div>
-        <h2>Download the extension</h2>
-        <p>
-          We've got more browsers in the pipeline. Please do let us know if
-          you've got a favourite you'd like us to prioritize.
-        </p>
+        <div className={styles.downloadText}>
+          <h2>Download the extension</h2>
+          <p>
+            We've got more browsers in the pipeline. Please do let us know if
+            you've got a favourite you'd like us to prioritize.
+          </p>
+        </div>
 
-        <div className={styles.browsers}>
-          <div>
+        <div className={styles.browsersContainer}>
+          <div className={styles.browser}>
+            <div className={styles.browserLogo}>
+              <img src="/images/logo-chrome.svg" alt="" />
+            </div>
             <h3>Add to Chrome</h3>
             <p>Minimum version 62</p>
+            <div>
+              <img src="/images/bg-dots.svg" alt="" />
+            </div>
+            <button>Add & Install Extension</button>
           </div>
-          <div>
+
+          <div className={styles.browser}>
+            <div className={styles.browserLogo}>
+              <img src="/images/logo-firefox.svg" alt="" />
+            </div>
             <h3>Add to Firefox</h3>
             <p>Minimum version 55</p>
+            <div>
+              <img src="/images/bg-dots.svg" alt="" />
+            </div>
+            <button>Add & Install Extension</button>
           </div>
-          <div>
+
+          <div className={styles.browser}>
+            <div className={styles.browserLogo}>
+              <img src="/images/logo-opera.svg" alt="" />
+            </div>
             <h3>Add to Opera</h3>
             <p>Minimum version 46</p>
+            <div>
+              <img src="/images/bg-dots.svg" alt="" />
+            </div>
+            <button>Add & Install Extension</button>
           </div>
         </div>
-        
       </div>
     </div>
   );
