3dfa5262c5326cf7d0215e86785141a01b85c635
Matheus <matheushenr941@gmail.com>
2022-03-22 19:12:03 -0300

    component features; fontes, alinhamento, botoes

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,18 +1,28 @@
-import styles from './features.module.scss'
+import styles from "./features.module.scss";
 
 export default function Features() {
   return (
     <div className={styles.component}>
-      <div>
+      <div className={styles.featuresText}>
         <h1>Features</h1>
-        <p>Our aim is to make quick and easy for you to acess your favourite websites. Your bookmark sync between your devices so you can acess them on go.</p>
+        <p>
+          Our aim is to make quick and easy for you to acess your favourite
+          websites. Your bookmark sync between your devices so you can acess
+          them on go.
+        </p>
       </div>
 
-      <div>
-        <div>Simple Bookmarking</div>
-        <div>Speedy Searching</div>
-        <div>Easy sharing</div>
+      <div className={styles.items}>
+        <div>
+          <p>Simple Bookmarking</p>
+        </div>
+        <div>
+          <p>Speedy Searching</p>
+        </div>
+        <div>
+          <p>Easy Sharing</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
