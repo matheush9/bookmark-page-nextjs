8645549c13a53b378eda934406bba3725c09f6c4
Matheus <matheushenr941@gmail.com>
2022-03-23 10:45:08 -0300

    Faq finalizado

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,23 +1,49 @@
 import styles from "./faq.module.scss";
+import Image from "next/image";
 
 export default function Faq() {
   return (
-    <div>
-      <div>
+    <div className={styles.component}>
+      <div className={styles.faqText}>
         <h2>Frequently Asked Questions</h2>
-        <p>Here are some of our FAQs. If you have any other questions you'd like answered please fell free to email us.</p>
+        <p>
+          Here are some of our FAQs. If you have any other questions you'd like
+          answered please fell free to email us.
+        </p>
       </div>
 
-      <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      </div>
+      <div className={styles.questions}>
+        <div className={styles.question}>
+          <h3>What is Bookmark</h3>
+          <div>
+            <img src="/images/icon-arrow.svg" alt="" />
+          </div>
+        </div>
 
-      <div>
-        <button>More Info</button>
+        <div className={styles.question}>
+          <h3>How can i request a new browser?</h3>
+          <div>
+            <img src="/images/icon-arrow.svg" alt="" />
+          </div>
+        </div>
+
+        <div className={styles.question}>
+          <h3>Is there a mobile app?</h3>
+          <div>
+            <img src="/images/icon-arrow.svg" alt="" />
+          </div>
+        </div>
+
+        <div className={styles.question}>
+          <h3>What about other Chromium browsers?</h3>
+          <div>
+            <img src="/images/icon-arrow.svg" alt="" />
+          </div>
+        </div>
       </div>
+      <div className={styles.btn}>
+          <button>More Info</button>
+        </div>
     </div>
   );
 }
