6948d3f3b767ce9a14aaa1f2593f765289956228
Matheus <matheushenr941@gmail.com>
2022-03-24 17:49:35 -0300

    Features tabs

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./features.module.scss";
+import Image from "next/image";
 
 export default function Features() {
   return (
@@ -21,6 +22,40 @@ export default function Features() {
         </div>
         <div>
           <p>Easy Sharing</p>
+        </div>
+      </div>
+
+      <div className={styles.features}>
+        
+        <div className={styles.simple}>
+          <div className={styles.container}>
+            <div>
+              <img src="/images/illustration-features-tab-1.svg" alt="" />
+            </div>
+            <div className={styles.blueBg}></div>
+          </div>
+          
+          <div className="styles.text">
+            <h3>Bookmark in one click</h3>
+            <p>
+              Organize your bookmarks however you like. Our simple drag-and-drop
+              interface gives you complete control over how you manage your
+              favorite sites.
+            </p>
+            <button>More Info</button>
+          </div>
+        </div>
+
+        <div className={styles.speedy}>
+          <div>
+            <img src="/images/illustration-features-tab-2.svg" alt="" />
+          </div>
+        </div>
+
+        <div className={styles.easy}>
+          <div>
+            <img src="/images/illustration-features-tab-3.svg" alt="" />
+          </div>
         </div>
       </div>
     </div>
