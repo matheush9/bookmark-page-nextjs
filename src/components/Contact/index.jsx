ff1c69fc61f21084799b2dbae34d99a331cf42d1
Matheus <matheushenr941@gmail.com>
2022-04-02 19:26:30 -0300

    all components responsive

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <div className={styles.stayUp}>
         <p>3 5, 0 0 0 + &nbsp;A L R E A D Y &nbsp; J O I N E D</p>
         <h2>Stay up-to-date with what we're doing</h2>
-        <div>
+        <div className={styles.email}>
           <input
             placeholder="Enter your email adress"
             type="email"
