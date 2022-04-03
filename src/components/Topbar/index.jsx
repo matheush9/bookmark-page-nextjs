6382583884ed8638d38367d05054b4f6a8353b38
Matheus <matheushenr941@gmail.com>
2022-04-02 22:39:49 -0300

    modal mobile

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
 import styles from "./topbar.module.scss";
 import Image from "next/image";
 
 export default function Topbar() {
+  const [stateHamburger, setStateHamburger] = useState(true);
+  const [stateClose, setStateClose] = useState(false);
+
+  const ChangeStateHamburger = () => {
+    setStateHamburger(false), setStateClose(true);
+  };
+
+  const ChangeStateClose = () => {
+    setStateHamburger(true), setStateClose(false);
+  };
+
   return (
     <div className={styles.component}>
       <div className={styles.topbar}>
@@ -9,11 +21,11 @@ export default function Topbar() {
           <img src="/images/logo-bookmark.svg" alt="" />
         </div>
         <div className={styles.navIcon}>
-          <div className={styles.hamburger}>
+          <div
+            onClick={ChangeStateHamburger}
+            className={stateHamburger ? styles.hamburger : styles.displayNone}
+          >
             <img src="/images/icon-hamburger.svg" alt="" />
-          </div>
-          <div className={styles.close}>
-            <img src="/images/icon-close.svg" alt="" />
           </div>
         </div>
 
@@ -22,6 +34,26 @@ export default function Topbar() {
           <p>PRICING</p>
           <p>CONTACT</p>
           <button>LOGIN</button>
+        </div>
+
+        <div className={stateClose ? styles.navbarMobile : styles.displayNone}>
+          <div className={styles.close}>
+            <img
+              onClick={ChangeStateClose}
+              className={stateClose ? styles.close : styles.displayNone}
+              src="/images/icon-close.svg"
+              alt=""
+            />
+          </div>
+          <div className={styles.wrapper}></div>
+          <p>FEATURES</p>
+          <div className={styles.wrapper}></div>
+          <p>PRICING</p>
+          <div className={styles.wrapper}></div>
+          <p>CONTACT</p>
+          <div className={styles.wrapper}></div>
+
+          <p className={styles.login}>LOGIN</p>
         </div>
       </div>
     </div>
